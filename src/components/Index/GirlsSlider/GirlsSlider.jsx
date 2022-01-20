22b42b807813436268f91579c424e9a29d1f8b14
notequal21 <kalorigen@gmail.com>
2022-01-20 22:10:13 +0300

    save

--- src/components/Index/GirlsSlider/GirlsSlider.jsx
+++ src/components/Index/GirlsSlider/GirlsSlider.jsx
@@ -1,13 +1,14 @@
 import style from './GirlsSlider.module.scss'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-
+import './sliderStyles.scss';
 
 let GirlsSlider = () => {
   return (
     <>
-      <div className={style.slider}>
+      <div className={`${style.slider} girlsSlider`}>
         <Swiper
+          grabCursor={true}
           spaceBetween={50}
           slidesPerView={'auto'}
           loop={true}
@@ -17,7 +18,7 @@ let GirlsSlider = () => {
         >
           <SwiperSlide className={style.slider__slide}>
             {({ isActive }) => (
-              <div className={style.sliderItem}>
+              <div className={`${style.sliderItem} ${isActive ? style.active : ''}`}>
                 <div className={style.sliderItem__img}>
 
                 </div>
@@ -47,7 +48,7 @@ let GirlsSlider = () => {
 
           <SwiperSlide className={style.slider__slide}>
             {({ isActive }) => (
-              <div className={style.sliderItem}>
+              <div className={`${style.sliderItem} ${isActive ? style.active : ''}`}>
                 <div className={style.sliderItem__img}>
 
                 </div>
@@ -75,7 +76,7 @@ let GirlsSlider = () => {
           </SwiperSlide>
           <SwiperSlide className={style.slider__slide}>
             {({ isActive }) => (
-              <div className={style.sliderItem}>
+              <div className={`${style.sliderItem} ${isActive ? style.active : ''}`}>
                 <div className={style.sliderItem__img}>
 
                 </div>
@@ -103,7 +104,35 @@ let GirlsSlider = () => {
           </SwiperSlide>
           <SwiperSlide className={style.slider__slide}>
             {({ isActive }) => (
-              <div className={style.sliderItem}>
+              <div className={`${style.sliderItem} ${isActive ? style.active : ''}`}>
+                <div className={style.sliderItem__img}>
+
+                </div>
+                <div className={`${style.sliderItem__content} ${isActive ? style.active : ''}`}>
+                  <div className={style.sliderItem__name}>
+                    Cool Girl #522
+                  </div>
+                  <div className={style.sliderItem__info}>
+                    <div className={style.sliderItem__infoItem}>
+                      Rarity needed
+                      <span>
+                        Rare+
+                      </span>
+                    </div>
+                    <div className={style.sliderItem__infoItem}>
+                      Hatch on
+                      <span>
+                        December 25th
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </SwiperSlide>
+          <SwiperSlide className={style.slider__slide}>
+            {({ isActive }) => (
+              <div className={`${style.sliderItem} ${isActive ? style.active : ''}`}>
                 <div className={style.sliderItem__img}>
 
                 </div>
